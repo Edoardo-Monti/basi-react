7ba48c23ba507a77faa1ef5759687fa2d7097f53
Edoardo Monti <edoardo.monti.bo@gmail.com>
2024-03-01 14:29:09 +0100

    prova con js plain a sostituire ReactDOM.render

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-function MainContent(){
-    return(
-        <p>Questo è il componente main</p>
-    );
-}
+// function MainContent(){
+//     return(
+//         <p>Questo è il componente main</p>
+//     );
+// }
 
-function NavBar(){
-    return(
-        <h1>NavBar</h1>
-    )
-}
+// function NavBar(){
+//     return(
+//         <h1>NavBar</h1>
+//     )
+// }
 
-function Footer(){
-    return(
-        <h3>Footer</h3>
-    )
-}
+// function Footer(){
+//     return(
+//         <h3>Footer</h3>
+//     )
+// }
 
 
-ReactDOM.render(
-<div>
-    <NavBar/>
-    <MainContent/>
-    <Footer/>
-</div>,
-document.getElementById("root"))
+// ReactDOM.render(
+// <div>
+//     <NavBar/>
+//     <MainContent/>
+//     <Footer/>
+// </div>,
+// document.getElementById("root"))
+const newElem = document.createElement("p")
+const box = document.getElementById("root")
+
+newElem.textContent = "ciao sono il nuovo contenuto"
+newElem.className = "paragraph" 
+
+
+box.appendChild(newElem)
+console.log(newElem)
